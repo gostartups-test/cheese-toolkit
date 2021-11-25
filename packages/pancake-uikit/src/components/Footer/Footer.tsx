085fdbb0c87f426286ec01fdf3c866c72eb87fd8
Todor <todor@gostartups.net>
2021-11-25 11:39:41 +0200

    Change to url in cheese price link.

--- packages/pancake-uikit/src/components/Footer/Footer.tsx
+++ packages/pancake-uikit/src/components/Footer/Footer.tsx
@@ -91,7 +91,7 @@ const MenuItem: React.FC<FooterProps> = ({
             </Box>
             <Button
               as="a"
-              href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
+              href="/swap?outputCurrency=0xf1c4BdAb1002f823493C41342FfBe456cf814570"
               target="_blank"
               scale="sm"
               endIcon={<ArrowForwardIcon color={lightColors.backgroundAlt} />}
