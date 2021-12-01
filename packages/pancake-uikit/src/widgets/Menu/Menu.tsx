95fc281c7ba040cc0945228aada7831d3fcfc947
Todor <todor@gostartups.net>
2021-12-01 15:04:12 +0200

    Languages hidden, colors changed, BUSDPrice icon changed.

--- packages/pancake-uikit/src/widgets/Menu/Menu.tsx
+++ packages/pancake-uikit/src/widgets/Menu/Menu.tsx
@@ -138,7 +138,7 @@ const Menu: React.FC<NavProps> = ({
                 <CakePrice cakePriceUsd={cakePriceUsd} />
               </Box>
             )}
-            <Box mt="4px">
+            {/* <Box mt="4px">
               <LangSelector
                 currentLang={currentLang}
                 langs={langs}
@@ -147,7 +147,7 @@ const Menu: React.FC<NavProps> = ({
                 color="textSubtle"
                 hideLanguage
               />
-            </Box>
+            </Box> */}
             {globalMenu} {userMenu}
           </Flex>
         </StyledNav>
